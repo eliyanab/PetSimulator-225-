44af118f97a2d8c4812dde87f6441f0963440588
Eliyana Brown <eliyanapearl@gmail.com>
2021-11-25 17:36:05 -0500

    algorithms 1.0

---
+++ js/cat.js
@@ -0,0 +1,84 @@
+var lovePoints = 0;
+var feed = 0;
+var pet = 0;
+var play = 0;
+var clean = 0;
+var sleep = 0;
+var speak = 0;
+
+
+$('#feed').click(function(){
+    lovePoints += 2;
+    feed += 1;
+    console.log(lovePoints + ", " + feed);
+    if(feed%5 == 0){
+        alert("Your pet needs to play");
+    }
+    if(lovePoints == 1000){
+        alert("Congrats, you maxed out the love bar! You and your pet are best friends! Keep taking care of your pet to earn even more points.");
+    }
+    else if(lovePoints%1000 == 0 && lovePoints != 0){
+        alert("Congrats, you've earned " + lovePoints + " points! Keep up the good work.");
+    }
+});
+$('#pet').click(function(){
+    lovePoints += 1;
+    pet += 1;
+    console.log(lovePoints + ", " + pet);
+    if(lovePoints == 1000){
+        alert("Congrats, you maxed out the love bar! You and your pet are best friends! Keep taking care of your pet to earn even more points.");
+    }
+    else if(lovePoints%1000 == 0 && lovePoints != 0){
+        alert("Congrats, you've earned " + lovePoints + " points! Keep up the good work.");
+    }
+});
+$('#play').click(function(){
+    lovePoints += 1;
+    play += 1;
+    console.log(lovePoints + ", " + play);
+    if(play%5 == 0){
+        alert("Your pet needs some sleep");
+    }
+    if(lovePoints == 1000){
+        alert("Congrats, you maxed out the love bar! You and your pet are best friends! Keep taking care of your pet to earn even more points.");
+    }
+    else if(lovePoints%1000 == 0 && lovePoints != 0){
+        alert("Congrats, you've earned " + lovePoints + " points! Keep up the good work.");
+    }
+});
+$('#clean').click(function(){
+    lovePoints -= 1;
+    clean += 1;
+    console.log(lovePoints + ", " + clean);
+    if(lovePoints == 1000){
+        alert("Congrats, you maxed out the love bar! You and your pet are best friends! Keep taking care of your pet to earn even more points.");
+    }
+    else if(lovePoints%1000 == 0 && lovePoints != 0){
+        alert("Congrats, you've earned " + lovePoints + " points! Keep up the good work.");
+    }
+});
+$('#sleep').click(function(){
+    lovePoints += 2;
+    sleep +=1;
+    console.log(lovePoints + ", " + sleep);
+    if(sleep%30 == 0){
+        alert("Your pet needs to play");
+    }
+    if(lovePoints == 1000){
+        alert("Congrats, you maxed out the love bar! You and your pet are best friends! Keep taking care of your pet to earn even more points.");
+    }
+    else if(lovePoints%1000 == 0 && lovePoints != 0){
+        alert("Congrats, you've earned " + lovePoints + " points! Keep up the good work.");
+    }
+});
+$('#speak').click(function(){
+    lovePoints += 1;
+    speak += 1;
+    console.log(lovePoints + ", " + speak);
+    if(lovePoints == 1000){
+        alert("Congrats, you maxed out the love bar! You and your pet are best friends! Keep taking care of your pet to earn even more points.");
+    }
+    else if(lovePoints%1000 == 0 && lovePoints != 0){
+        alert("Congrats, you've earned " + lovePoints + " points! Keep up the good work.");
+    }
+});
